98ed66af457ccac06b4a78b6ac54a8b4010e89da
Kritik Nagar <nagarkritik@gmail.com>
2022-07-12 16:48:47 +0530

    getting messages from database

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,11 @@ const sequelize = require('./util/database')
 
 const userRoutes = require('./routes/user')
 const gropuRoutes = require('./routes/group')
+const messageRoutes = require('./routes/message')
 
 const User = require('./models/user')
 const Group = require('./models/group')
+const Message = require('./models/message')
 
 
 const app = express()
@@ -21,10 +23,13 @@ app.use(bodyParser.json())
 
 app.use(userRoutes)
 app.use(gropuRoutes)
+app.use(messageRoutes)
 
 User.hasMany(Group)
 Group.belongsTo(User)
 
+User.hasMany(Message)
+Message.belongsTo(User)
 
 sequelize.sync()
 .then((res)=>{
